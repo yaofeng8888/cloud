3cf64643c6c3a1f980972b35b8be9852a3df4b04
yaofeng <yaofeng@tourage>
2021-01-12 14:35:25 +0800

    hystrix 服务调用方通用服务降级

--- cloud-consumer-order-hystrix-openfeign/src/main/java/com/george/springcloud/controller/PaymentHystrixController.java
+++ cloud-consumer-order-hystrix-openfeign/src/main/java/com/george/springcloud/controller/PaymentHystrixController.java
@@ -3,6 +3,7 @@ package com.george.springcloud.controller;
 import com.george.springcloud.feign.PaymentHystrixService;
 import com.george.springcloud.model.ApiResult;
 import com.george.springcloud.model.Payment;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
@@ -19,12 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yaofeng 2020-12-14 17:42
  */
 @RestController
+@DefaultProperties(defaultFallback = "commonFallback")//通用的服务降级方法
 public class PaymentHystrixController {
 	@Autowired
 	private PaymentHystrixService orderService;
 
 	@RequestMapping("/getOne/{id}")
+	@HystrixCommand
 	public String getOne(@PathVariable("id") Long id){
+		int i = 10/0;
 		return orderService.getPayment_OK(id);
 	}
 
@@ -43,5 +47,9 @@ public class PaymentHystrixController {
 	}
 	public String timeOutFallback(@PathVariable("id") Long id){
 		return "Hystrix 服务正忙，请稍后再试！编号="+id;
+	}
+
+	public String commonFallback(){
+		return "通用服务降级，服务正忙!";
 	}
 }
