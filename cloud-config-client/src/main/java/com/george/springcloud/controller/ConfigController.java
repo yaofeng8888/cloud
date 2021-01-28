e0a35522100fa29107a44f8f41a367a35fb1d202
yaofeng <yaofeng@tourage>
2021-01-28 16:28:22 +0800

    cloudConfig 动态刷新

--- cloud-config-client/src/main/java/com/george/springcloud/controller/ConfigController.java
+++ cloud-config-client/src/main/java/com/george/springcloud/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.george.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yaofeng 2021/1/28 4:10 下午
  */
 @RestController
+@RefreshScope
 public class ConfigController {
 
 	@Value("${config.info}")
