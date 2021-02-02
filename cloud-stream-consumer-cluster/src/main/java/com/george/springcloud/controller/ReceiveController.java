227188aeb46d96f933a8237de3fd98e479d888ef
yaofeng <yaofeng@tourage>
2021-02-02 16:42:44 +0800

    SpringCloudStream消息驱动 重复消费问题

---
+++ cloud-stream-consumer-cluster/src/main/java/com/george/springcloud/controller/ReceiveController.java
@@ -0,0 +1,35 @@
+package com.george.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ReceiveController.
+ *
+ * @author yaofeng 2021/2/2 3:26 下午
+ */
+@RestController
+@EnableBinding(Sink.class)
+public class ReceiveController {
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	private String mess;
+
+	@StreamListener(Sink.INPUT)
+	public void getMessage(Message<String> message){
+		mess = message.getPayload()+serverPort;
+	}
+
+	@GetMapping("getMessage")
+	public String Message(){
+		return mess;
+	}
+}
